85fe5be7160159843be3902fd040303168066d45
Tyler Varnum <tylervarnum9@gmail.com>
2025-10-10 15:27:26 -0400

    Added pygame dependency and basic window init

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 # external imports
+import pygame
+import pygame.locals
+import sys
 
 # internal imports
 import items
@@ -6,8 +9,18 @@ import character
 
 
 def main():
-    main_character = character.Character()
-    main_character.name = "Tyler"
+    pygame.init()
+
+    display = pygame.display.set_mode((400, 600))
+
+    while True:
+        display.fill((255, 0, 255))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                pygame.quit()
+                sys.exit()
 
 
 if __name__ == "__main__":
