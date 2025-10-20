d5304415bd9e08325b0a38e543c2253971098639
Tyler Varnum <tylervarnum9@gmail.com>
2025-10-20 14:31:54 -0400

    added character class __init__ function

--- src/character.py
+++ src/character.py
@@ -3,4 +3,16 @@ class Character:
     items = []
     name = ""
     description = ""
-    character_design={"color":(0,0,0),"left":50,"top":100,"width":10,"height":10}
+    character_design = {
+        "color": (0, 0, 0),
+        "left": 50,
+        "top": 100,
+        "width": 10,
+        "height": 10,
+    }
+
+    def __init__(self, _health, _starting_inven, _name, _description):
+        self.current_health = _health
+        self.items = _starting_inven
+        self.name = _name
+        self.description = _description
