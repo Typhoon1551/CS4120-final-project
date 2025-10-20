441bca4d467849586b991bfa99bf6fb697862d4b
Zebaniah <zebaniah.whitmore@gmail.com>
2025-10-20 10:50:27 -0400

    Added some cool stuff

--- src/character.py
+++ src/character.py
@@ -3,3 +3,4 @@ class Character:
     items = []
     name = ""
     description = ""
+    character_design={"color":(0,0,0),"left":50,"top":100,"width":10,"height":10}
--- src/main.py
+++ src/main.py
@@ -7,20 +7,37 @@ import sys
 import items
 import character
 
-
 def main():
     pygame.init()
-
-    display = pygame.display.set_mode((400, 600))
+    display_size=pygame.display.Info()
+    display = pygame.display.set_mode((display_size.current_w, display_size.current_h))
 
     while True:
-        display.fill((255, 0, 255))
-        pygame.display.update()
+        display.fill((225, 225, 225))
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_UP]==True or key[pygame.K_w] == True:
+            character.Character.character_design["top"]-=10
+        elif key[pygame.K_DOWN]==True or key[pygame.K_s] == True:
+            character.Character.character_design["top"]+=10
+        elif key[pygame.K_LEFT]==True or key[pygame.K_a] == True:
+            character.Character.character_design["left"]-=10
+        elif key[pygame.K_RIGHT]==True or key[pygame.K_d] == True:
+            character.Character.character_design["left"]+=10
+        
+        characterbuild = character.Character.character_design
+
+        pygame.draw.rect(
+            display,
+            characterbuild["color"],
+            pygame.Rect(characterbuild["left"],characterbuild["top"],int(characterbuild["width"]),int(characterbuild["height"])))
 
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
                 pygame.quit()
                 sys.exit()
+        pygame.display.update()
+
 
 
 if __name__ == "__main__":
